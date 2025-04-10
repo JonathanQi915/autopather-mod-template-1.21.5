49db04151d9596cc639c0511dc8ddcaf1c39b14d
jonathanqi <qi.jo@northeastern.edu>
2025-04-10 17:18:34 -0400

    Initial commit

---
+++ src/main/java/neu/authopathermod/AutopatherModClient.java
@@ -0,0 +1,10 @@
+package neu.authopathermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutopatherModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/neu/authopathermod/AutopatherModDataGenerator.java
@@ -0,0 +1,11 @@
+package neu.authopathermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutopatherModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
